9a090547eb73c123578b7c76b1a77035588538c8
Yeasir H <yeasir01@gmail.com>
2020-03-10 16:49:48 -0700

    added addtional markdown data

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,8 +1,18 @@
 function generateMarkdown(data) {
   return `
 # ${data.title}
-
+${data.description}
+### Table of contents
+${data.tableOfContents}
+### Installation
+${data.install}
+### Usage
+${data.usage}
+### License
+${data.license}
 `;
 }
 
 module.exports = generateMarkdown;
+
+
