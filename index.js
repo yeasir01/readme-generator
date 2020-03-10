7ab5414b8400f804ec7efe4d99b6f9dbb00a22af
Yeasir H <yeasir01@gmail.com>
2020-03-10 12:56:10 -0700

    added addtional functions

--- index.js
+++ index.js
@@ -1,12 +1,43 @@
-const questions = [
+const fs = require("fs");
+const axios = require("axios");
+const inquirer = require("inquirer");
 
-];
+inquirer.prompt([
+    {
+        type: "input",
+        message: "What is the name of your project?",
+        name: "title"
+    },
+    {
+        type: "input",
+        message: "Project Description?",
+        name: "description"
+    },
+    {
+        type: "input",
+        message: "table of contents",
+        name: "tableOfContents"
+    }
+]).then(data => {
+    writeToFile(data);
+});
 
-function writeToFile(fileName, data) {
+function writeToFile(data) {
+    
+    var filename = data.title.toLowerCase().split(' ').join('') + "-README.md";
+
+    fs.writeFile(filename, JSON.stringify(data, null, '\t'), function(err) {
+
+        if (err) {
+          return console.log(err);
+        }
+    
+        console.log("Readme successfully generated!");
+    });
 }
 
 function init() {
-
+  
 }
 
 init();
