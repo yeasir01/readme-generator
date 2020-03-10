7d43c408f2c94656276873e7bd5cc901531a5e78
Yeasir H <yeasir01@gmail.com>
2020-03-10 12:56:40 -0700

    build call

--- utils/api.js
+++ utils/api.js
@@ -1,6 +1,22 @@
 const api = {
-  getUser(username) {
+  getUser( {username} ) {
+    const queryUrl = `https://api.github.com/users/${username}/repos?per_page=100`;
 
+    axios.get(queryUrl).then(function(res) {
+      const repoNames = res.data.map(function(repo) {
+        return repo.name;
+      });
+
+      const repoNamesStr = repoNames.join("\n");
+
+      fs.writeFile("repos.txt", repoNamesStr, function(err) {
+        if (err) {
+          throw err;
+        }
+
+        console.log(`Saved ${repoNames.length} repos`);
+      });
+    });
   }
 };
 
