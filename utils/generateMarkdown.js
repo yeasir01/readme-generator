a0139360930dc8e650680d2e3cca0d06882b85bf
Yeasir H <yeasir01@gmail.com>
2020-03-11 02:13:29 -0700

    built addional markdown

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -2,14 +2,23 @@ function generateMarkdown(data) {
   return `
 # ${data.title}
 ${data.description}
-### Table of contents
-${data.tableOfContents}
-### Installation
+## Table of contents
+* [Installation](#Installation)
+* [Usage](#Usage)
+* [License](#License)
+* [Contributors](#Contributors)
+* [Tests](#tests)
+## Installation
 ${data.install}
-### Usage
+## Usage
 ${data.usage}
-### License
-${data.license}
+## License
+This project is licensed under [${data.license}](https://choosealicense.com/licenses/).
+## Contributors
+[![forthebadge](https://forthebadge.com/images/badges/built-by-hipsters.svg)](https://forthebadge.com)
+***
+
+_This file was generated using a [Readme Generator](https://github.com/yeasir01/readme-generator) developed by Yeasir H._
 `;
 }
 
